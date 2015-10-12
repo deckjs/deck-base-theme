dc2d4acd14f0cd3d6429cf5839179e3df2f4c52b
Cristian Kiss <cristian.kiss@nearform.com>
2015-10-12 15:36:01 +0300

    added standard + formatting

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,32 @@
-var gulp = require('gulp'),
-  clean = require('gulp-clean'),
-  header = require('gulp-header'),
-  rename = require('gulp-rename'),
-  uglify = require('gulp-uglify'),
-  stylus = require('gulp-stylus'),
-  autoprefixer = require('gulp-autoprefixer'),
-  csso = require('gulp-csso'),
-  pkg = require('./package.json'),
-  browserify = require('gulp-browserify'),
-  path = require('path'),
-  template = require('lodash').template;
+var gulp = require('gulp')
+var clean = require('gulp-clean')
+var header = require('gulp-header')
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
+var stylus = require('gulp-stylus')
+var autoprefixer = require('gulp-autoprefixer')
+var csso = require('gulp-csso')
+var pkg = require('./package.json')
+var browserify = require('gulp-browserify')
+var template = require('lodash').template
 
-gulp.task('default', ['clean', 'compile']);
-gulp.task('compile', ['stylus', 'browserify:lib']);
+gulp.task('default', ['clean', 'compile'])
+gulp.task('compile', ['stylus', 'browserify:lib'])
 
-gulp.task('clean', ['clean:browserify', 'clean:stylus']);
-gulp.task('clean:browserify', ['clean:browserify:lib']);
+gulp.task('clean', ['clean:browserify', 'clean:stylus'])
+gulp.task('clean:browserify', ['clean:browserify:lib'])
 
-gulp.task('clean:browserify:lib', function() {
+gulp.task('clean:browserify:lib', function () {
   return gulp.src(['dist'], { read: false })
-    .pipe(clean());
-});
+    .pipe(clean())
+})
 
-
-gulp.task('clean:stylus', function() {
+gulp.task('clean:stylus', function () {
   return gulp.src(['css'], { read: false })
-    .pipe(clean());
-});
+    .pipe(clean())
+})
 
-
-gulp.task('stylus', ['clean:stylus'], function() {
+gulp.task('stylus', ['clean:stylus'], function () {
   return gulp.src('styl/main.styl')
     .pipe(stylus({
       'include css': true,
@@ -37,12 +34,12 @@ gulp.task('stylus', ['clean:stylus'], function() {
     }))
     .pipe(autoprefixer('last 2 versions'))
     .pipe(csso())
-    .pipe(gulp.dest('css'));
-});
+    .pipe(gulp.dest('css'))
+})
 
-gulp.task('browserify', ['browserify:lib']);
+gulp.task('browserify', ['browserify:lib'])
 
-gulp.task('browserify:lib', ['clean:browserify:lib', 'stylus'], function() {
+gulp.task('browserify:lib', ['clean:browserify:lib', 'stylus'], function () {
   return gulp.src('src/theme.js')
     .pipe(browserify({transform: ['brfs'], standalone: 'theme'}))
     .pipe(header(template([
@@ -57,7 +54,7 @@ gulp.task('browserify:lib', ['clean:browserify:lib', 'stylus'], function() {
     .pipe(uglify())
     .pipe(header(template([
       '/*! <%= name %> v<%= version %> ',
-      '© <%= new Date().getFullYear() %> <%= author.name %> */',
+      '© <%= new Date().getFullYear() %> <%= author.name %> */'
     ].join(''), pkg)))
-    .pipe(gulp.dest('.'));
-});
+    .pipe(gulp.dest('.'))
+})
